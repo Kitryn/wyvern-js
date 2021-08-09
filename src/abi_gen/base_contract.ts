f22e5dab8d66f8ea3e7a5a0783fcf6c42b884650
Kitryn <snowfennard@gmail.com>
2021-08-09 18:39:33 +0800

    formatting

--- src/abi_gen/base_contract.ts
+++ src/abi_gen/base_contract.ts
@@ -1,13 +1,15 @@
-import {TxData, TxDataPayable} from '@0xproject/types';
-import * as _ from 'lodash';
-import * as Web3 from 'web3';
+import { TxData, TxDataPayable } from "@0xproject/types";
+import * as _ from "lodash";
+import * as Web3 from "web3";
 
 export class BaseContract {
     protected web3ContractInstance: Web3.ContractInstance;
     protected defaults: Partial<TxData>;
-    protected async applyDefaultsToTxDataAsync<T extends TxData|TxDataPayable>(
+    protected async applyDefaultsToTxDataAsync<
+        T extends TxData | TxDataPayable
+    >(
         txData: T,
-        estimateGasAsync?: (txData: T) => Promise<number>,
+        estimateGasAsync?: (txData: T) => Promise<number>
     ): Promise<TxData> {
         // Gas amount sourced with the following priorities:
         // 1. Optional param passed in to public method call
@@ -20,13 +22,19 @@ export class BaseContract {
             // HACK: TS can't prove that T is spreadable.
             // Awaiting https://github.com/Microsoft/TypeScript/pull/13288 to be merged
         };
-        if (_.isUndefined(txDataWithDefaults.gas) && !_.isUndefined(estimateGasAsync)) {
+        if (
+            _.isUndefined(txDataWithDefaults.gas) &&
+            !_.isUndefined(estimateGasAsync)
+        ) {
             const estimatedGas = await estimateGasAsync(txData);
             txDataWithDefaults.gas = estimatedGas;
         }
         return txDataWithDefaults;
     }
-    constructor(web3ContractInstance: Web3.ContractInstance, defaults: Partial<TxData>) {
+    constructor(
+        web3ContractInstance: Web3.ContractInstance,
+        defaults: Partial<TxData>
+    ) {
         this.web3ContractInstance = web3ContractInstance;
         this.defaults = defaults;
     }
