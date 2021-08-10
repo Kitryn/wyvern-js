1e1f20fb0048661ebc8fa0502b575073dff0234a
Kitryn <snowfennard@gmail.com>
2021-08-10 11:43:58 +0800

    tests passing

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-yarn run mocha test/*.ts --require ts-node/register
+TS_NODE_FILES=true yarn run mocha test/*.ts --require ts-node/register
 
 exit 0
--- test/basic.ts
+++ test/basic.ts
@@ -1,140 +1,140 @@
-import {
-  assert,
-} from 'chai';
+import { assert } from "chai";
+import { describe, it } from "mocha";
 
-import {
-  suite,
-  test,
-} from 'mocha-typescript';
+/// <reference path="../src/globals.d.ts"
+import * as ethABI from "ethereumjs-abi";
 
-import * as ethABI from 'ethereumjs-abi';
+import { WyvernProtocol } from "../src/index";
+import { AbiType, FunctionInputKind, StateMutability } from "../src/types";
 
-import {
-  WyvernProtocol,
-} from '../src/index';
-import {
-  AbiType,
-  FunctionInputKind,
-  StateMutability,
-} from '../src/types';
-
-import * as ordersAndHashesJSON from './ordersAndHashes.json';
+// import * as ordersAndHashesJSON from "./ordersAndHashes.json";
+const ordersAndHashesJSON = require("./ordersAndHashes.json");
 const ordersAndHashes = ordersAndHashesJSON as any;
 
-suite('basic', () => {
-  test('Null address is correct', () => {
-    assert.equal(WyvernProtocol.NULL_ADDRESS, '0x0000000000000000000000000000000000000000');
-  });
-
-  test('Max uint256 is correct', () => {
-    assert.equal(WyvernProtocol.MAX_UINT_256.toString(), '115792089237316195423570985008687907853269984665640564039457584007913129639935');
-  });
-
-  ordersAndHashes.map((orderAndHash: any, index: number) => {
-    test('Order #' + index + ' hash is correct', () => {
-      const hash = WyvernProtocol.getOrderHashHex(orderAndHash.order);
-      assert.equal(hash, orderAndHash.hash);
+describe("basic", () => {
+    it("Null address is correct", () => {
+        assert.equal(
+            WyvernProtocol.NULL_ADDRESS,
+            "0x0000000000000000000000000000000000000000"
+        );
     });
-  });
 
-  test('First replacementPattern encoding is correct', () => {
-    const annotatedABI = {
-      type: AbiType.Function,
-      name: 'testFunction',
-      target: '',
-      inputs: [
-      ],
-      outputs: [
-      ],
-      constant: false,
-      stateMutability: StateMutability.Payable,
-      payable: true,
-    };
-    const encoded = WyvernProtocol.encodeReplacementPattern(annotatedABI);
-    assert.equal(encoded, '0x00000000');
-    const methodID = '0x' + ethABI.methodID('testFunction', []).toString('hex');
-    assert.equal(methodID, '0xe16b4a9b');
-  });
+    it("Max uint256 is correct", () => {
+        assert.equal(
+            WyvernProtocol.MAX_UINT_256.toString(),
+            "115792089237316195423570985008687907853269984665640564039457584007913129639935"
+        );
+    });
 
-  test('Second complex replacementPattern encoding is correct', () => {
-    const annotatedABI = {
-      type: AbiType.Function,
-      name: 'testFunction',
-      target: '',
-      inputs: [
-        {
-          name: 'index',
-          type: 'uint256',
-          kind: FunctionInputKind.Replaceable,
-        },
-      ],
-      outputs: [
-      ],
-      constant: false,
-      stateMutability: StateMutability.Payable,
-      payable: true,
-    };
-    const encoded = WyvernProtocol.encodeReplacementPattern(annotatedABI);
-    assert.equal(encoded, '0x00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff');
-  });
+    ordersAndHashes.map((orderAndHash: any, index: number) => {
+        it("Order #" + index + " hash is correct", () => {
+            const hash = WyvernProtocol.getOrderHashHex(orderAndHash.order);
+            assert.equal(hash, orderAndHash.hash);
+        });
+    });
 
-  test('Third replacementPattern encoding is correct', () => {
-    const annotatedABI = {
-      type: AbiType.Function,
-      name: 'testFunction',
-      target: '',
-      inputs: [
-        {
-          name: 'index',
-          type: 'uint256',
-          kind: FunctionInputKind.Replaceable,
-        },
-        {
-          name: 'address',
-          type: 'address',
-          kind: FunctionInputKind.Owner,
-        },
-      ],
-      outputs: [
-      ],
-      constant: false,
-      stateMutability: StateMutability.Payable,
-      payable: true,
-    };
-    const encoded = WyvernProtocol.encodeReplacementPattern(annotatedABI);
-    assert.equal(encoded, '0x00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0000000000000000000000000000000000000000000000000000000000000000');
-  });
+    it("First replacementPattern encoding is correct", () => {
+        const annotatedABI = {
+            type: AbiType.Function,
+            name: "testFunction",
+            target: "",
+            inputs: [],
+            outputs: [],
+            constant: false,
+            stateMutability: StateMutability.Payable,
+            payable: true,
+        };
+        const encoded = WyvernProtocol.encodeReplacementPattern(annotatedABI);
+        assert.equal(encoded, "0x00000000");
+        const methodID =
+            "0x" + ethABI.methodID("testFunction", []).toString("hex");
+        assert.equal(methodID, "0xe16b4a9b");
+    });
 
-  test('Fourth replacementPattern encoding is correct', () => {
-    const annotatedABI = {
-      type: AbiType.Function,
-      name: 'testFunction',
-      target: '',
-      inputs: [
-        {
-          name: 'index',
-          type: 'uint256',
-          kind: FunctionInputKind.Replaceable,
-        },
-        {
-          name: 'addr',
-          type: 'address',
-          kind: FunctionInputKind.Owner,
-        },
-        {
-          name: 'bytes',
-          type: 'bytes32',
-          kind: FunctionInputKind.Replaceable,
-        },
-      ],
-      outputs: [
-      ],
-      constant: false,
-      stateMutability: StateMutability.Payable,
-      payable: true,
-    };
-    const encoded = WyvernProtocol.encodeReplacementPattern(annotatedABI);
-    assert.equal(encoded, '0x00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0000000000000000000000000000000000000000000000000000000000000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff');
-  });
+    it("Second complex replacementPattern encoding is correct", () => {
+        const annotatedABI = {
+            type: AbiType.Function,
+            name: "testFunction",
+            target: "",
+            inputs: [
+                {
+                    name: "index",
+                    type: "uint256",
+                    kind: FunctionInputKind.Replaceable,
+                },
+            ],
+            outputs: [],
+            constant: false,
+            stateMutability: StateMutability.Payable,
+            payable: true,
+        };
+        const encoded = WyvernProtocol.encodeReplacementPattern(annotatedABI);
+        assert.equal(
+            encoded,
+            "0x00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+        );
+    });
 
+    it("Third replacementPattern encoding is correct", () => {
+        const annotatedABI = {
+            type: AbiType.Function,
+            name: "testFunction",
+            target: "",
+            inputs: [
+                {
+                    name: "index",
+                    type: "uint256",
+                    kind: FunctionInputKind.Replaceable,
+                },
+                {
+                    name: "address",
+                    type: "address",
+                    kind: FunctionInputKind.Owner,
+                },
+            ],
+            outputs: [],
+            constant: false,
+            stateMutability: StateMutability.Payable,
+            payable: true,
+        };
+        const encoded = WyvernProtocol.encodeReplacementPattern(annotatedABI);
+        assert.equal(
+            encoded,
+            "0x00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0000000000000000000000000000000000000000000000000000000000000000"
+        );
+    });
+
+    it("Fourth replacementPattern encoding is correct", () => {
+        const annotatedABI = {
+            type: AbiType.Function,
+            name: "testFunction",
+            target: "",
+            inputs: [
+                {
+                    name: "index",
+                    type: "uint256",
+                    kind: FunctionInputKind.Replaceable,
+                },
+                {
+                    name: "addr",
+                    type: "address",
+                    kind: FunctionInputKind.Owner,
+                },
+                {
+                    name: "bytes",
+                    type: "bytes32",
+                    kind: FunctionInputKind.Replaceable,
+                },
+            ],
+            outputs: [],
+            constant: false,
+            stateMutability: StateMutability.Payable,
+            payable: true,
+        };
+        const encoded = WyvernProtocol.encodeReplacementPattern(annotatedABI);
+        assert.equal(
+            encoded,
+            "0x00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0000000000000000000000000000000000000000000000000000000000000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+        );
+    });
 });
